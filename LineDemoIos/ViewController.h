a42f973c61a73578eaaac8ad44eabb6ba908f28d
SoberTong <15821589161@163.com>
2017-04-05 19:33:19 +0800

    Initial Commit

---
+++ LineDemoIos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LineDemoIos
+//
+//  Created by Avidly on 2017/4/5.
+//  Copyright © 2017年 Avidly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LineDemoIos/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LineDemoIos
+//
+//  Created by Avidly on 2017/4/5.
+//  Copyright © 2017年 Avidly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
